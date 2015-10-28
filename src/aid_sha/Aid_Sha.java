9a5d1d601f868661149ceab7ade1e19393acda74
ChadiIR <ChadiIR@130.83.114.26>
2015-10-28 19:59:25 +0100

    commit 2

--- src/aid_sha/Aid_Sha.java
+++ src/aid_sha/Aid_Sha.java
@@ -15,7 +15,7 @@ public class Aid_Sha {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     System.out.println();   // TODO code application logic here
     }
     
 }
